e59818e43809114c7cb90080ed096740bfef9ecd
Pnivchuk <andrii.pnivchuk@haymarket.in.ua>
2021-09-04 16:51:25 +0300

    build month array with filled first and last weeks of month

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -43,13 +43,46 @@ export default {
         }
 
         if(day.getDay() === 0) weekIndex++; // if last day was Sunday - go to the next week
-
       }
+
+      const fillDaysBefore = () => { // fill first week with days from previous month
+        const firstWeekOfMonth = month[0];
+        const firstDayOfWeek = firstWeekOfMonth[0].day;
+
+        if(firstDayOfWeek.getDay() !== 1) { // if first day of week is not Monday
+          firstWeekOfMonth.unshift(
+            {
+              day: new Date(firstDayOfWeek.getFullYear(), firstDayOfWeek.getMonth(), firstDayOfWeek.getDate() - 1),
+              isCurrentMonth: false
+            }
+          )
+          fillDaysBefore();
+        }
+      }
+
+      const fillDaysAfter = () => { // fill last week with days from next month
+        const lastWeekOfMonth = month[month.length - 1];
+        const lastDayOfWeek = lastWeekOfMonth[lastWeekOfMonth.length - 1].day;
+
+        if(lastDayOfWeek.getDay() !== 0) { // if last day of week is not Sunday
+          lastWeekOfMonth.push(
+              {
+                day: new Date(lastDayOfWeek.getFullYear(), lastDayOfWeek.getMonth(), lastDayOfWeek.getDate() + 1),
+                isCurrentMonth: false
+              }
+          )
+          fillDaysAfter();
+        }
+      }
+
+      fillDaysBefore();
+      fillDaysAfter();
+
       return month;
     }
   },
   mounted() {
-    this.month = this.buildMonthArray(new Date());
+    this.month = this.buildMonthArray(new Date())
   }
 }
 </script>
