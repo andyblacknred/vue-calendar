08111e8d1887a57d00f72c91323dbab9d9a25a7f
Pnivchuk <andrii.pnivchuk@haymarket.in.ua>
2021-09-04 13:32:12 +0300

    changed method buildMonthArray to build weeks

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -16,22 +16,40 @@ export default {
     }
   },
   methods: {
-    renderMonth(date) {
-      const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
+    buildMonthArray(date) {
+
+      const firstDayOfMonth =
+          new Date(
+              date.getFullYear(),
+              date.getMonth(),
+              1
+          )
       const month = [];
+
       for
       (
-          let day = firstDayOfMonth;
-          day.getMonth() === firstDayOfMonth.getMonth();
-          day = new Date(day.getFullYear(), day.getMonth(), day.getDate() + 1)
+          let day = firstDayOfMonth, weekIndex = 0;
+          day.getMonth() === firstDayOfMonth.getMonth(); // check, is still on current month
+          day = new Date(day.getFullYear(), day.getMonth(), day.getDate() + 1) // go to the next day
       ) {
-        month.push(day);
+
+        const dayObject = {day, isCurrentMonth: true};
+
+        if(month[weekIndex]) { // write to current week
+          month[weekIndex].push(dayObject);
+        } else { // create new week
+          month[weekIndex] = [];
+          month[weekIndex].push(dayObject);
+        }
+
+        if(day.getDay() === 0) weekIndex++; // if last day was Sunday - go to the next week
+
       }
       return month;
     }
   },
   mounted() {
-    this.month = this.renderMonth(new Date());
+    this.month = this.buildMonthArray(new Date());
   }
 }
 </script>
